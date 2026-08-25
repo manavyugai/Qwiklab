b5492c03fabb86955bfbf8c0726590430d291f1b
manavyugai <manavyugai@gmail.com>
2026-08-25 09:17:09 +0530

    Delete Create and Manage Cloud SQL for PostgreSQL Instances_ Challenge Lab directory

--- Create and Manage Cloud SQL for PostgreSQL Instances_ Challenge Lab/Hi.sh
+++
@@ -1,8 +0,0 @@
-# Variables auto-fetch
-export PROJECT_ID=$(gcloud config get-value project)
-export VM_NAME=$(gcloud compute instances list --format="value(name)" | grep postgres)
-export ZONE=$(gcloud compute instances list --filter="name=$VM_NAME" --format="value(zone)")
-export REGION=${ZONE%-*}
-
-# Fixed promote command with location in resource path
-gcloud datamigration migration-jobs promote projects/$PROJECT_ID/locations/$REGION/migrationJobs/migration-job --quiet
